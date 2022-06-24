42fab268f08d9e028731814dcff518031802ce80
SOUMEN BARAT <58267870+SoumenBarat@users.noreply.github.com>
2022-06-24 11:25:59 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,20 @@
+// Java program to print MUltiplication of two floating point Number.
+
+class Multiply {
+
+    public static void main(String[] args) {
+
+        // f is to ensures that numbers are float DATA TYPE 
+
+        float first = 2.5f;
+        float second = 3.5f;
+
+        // to store the multiplied value
+
+        float product = first * second;
+
+        //to print the multiply of Two Number
+
+        System.out.println("The multiply of Two Number is: " + product);
+    }
+}
