2ba4d8d2fa60c2fb9d5ce43f9171ece7002348cf
SOUMEN BARAT <58267870+SoumenBarat@users.noreply.github.com>
2022-06-24 11:26:42 +0530

    Add files via upload

---
+++ Multiplytwonumbers.java
@@ -0,0 +1,19 @@
+//Java program to print Multiplication of two numbers
+ 
+class Multiplytwonumbers {
+ public static void main(String[] args) {
+
+     //variables
+        
+      int a = 15;
+      int b = 25;
+
+     // Multiplication     
+
+      int multi = a*b;
+
+     //to print the Multiplication of a & b
+
+      System.out.println("Multiplication of a & b : "+multi);
+   }
+}
