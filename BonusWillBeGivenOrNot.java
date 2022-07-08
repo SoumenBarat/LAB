2d7e6b463fd312f0eacab8c1fd2d9d56f9501ddc
SOUMEN BARAT <58267870+SoumenBarat@users.noreply.github.com>
2022-07-08 13:00:41 +0530

    Update BonusWillBeGivenOrNot.java

--- BonusWillBeGivenOrNot.java
+++ BonusWillBeGivenOrNot.java
@@ -3,13 +3,13 @@ package JavaProgram;
 import java.util.Scanner;
 public class BonusWillBeGivenOrNot {
 	public static void main(String[] args) {
-		int joiningyear,currentyear ,yearofservice;
-		Scanner in = new Scanner(System.in);
-             System.out.println("Enter the joining year ");
-        joiningyear=in.nextInt();
-			 System.out.println("Enter the current year "); 
-		currentyear=in.nextInt();
-		     yearofservice=currentyear-joiningyear;
+               int joiningyear,currentyear ,yearofservice;
+               Scanner in = new Scanner(System.in);
+               System.out.println("Enter the joining year ");
+               joiningyear=in.nextInt();
+	       System.out.println("Enter the current year "); 
+	       currentyear=in.nextInt();
+	       yearofservice=currentyear-joiningyear;
 		 if(yearofservice>=3) {
 			 System.out.println("year of service is "+yearofservice+" years so 2500 will be given to Employee");
 		 }
