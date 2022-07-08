77fabf8948aaf05e7521eb0a45c288cb9d815b80
SOUMEN BARAT <58267870+SoumenBarat@users.noreply.github.com>
2022-07-08 13:02:22 +0530

    Update consonantOrVowel.java

--- consonantOrVowel.java
+++ consonantOrVowel.java
@@ -4,7 +4,7 @@ public class consonantOrVowel {
 
 	    public static void main(String[] args) {
 
-	        char k = '9';
+	        char k = 'o';
 
 	        if(k == 'a' || k == 'e' || k == 'i' || k == 'o' || k == 'u' )
 	            System.out.println(k+ " is vowel");
